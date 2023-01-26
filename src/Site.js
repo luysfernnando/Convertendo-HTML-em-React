84aeca7358745d173bb1d7ba3e7fe852d6db0bd9
Luys Fernnando <luysfernnando@gmail.com>
2023-01-26 08:39:20 -0300

    Tags <input> Fechadas
    Em React é necessário Fechar as tags
    <input/>

--- src/Site.js
+++ src/Site.js
@@ -242,15 +242,15 @@ const Site = props => {
       <form action="/action_page.php" target="_blank">
         <div class="w3-section">
           <label>Name</label>
-          <input class="w3-input w3-border" type="text" name="Name" required>
+          <input class="w3-input w3-border" type="text" name="Name" required/>
         </div>
         <div class="w3-section">
           <label>Email</label>
-          <input class="w3-input w3-border" type="text" name="Email" required>
+          <input class="w3-input w3-border" type="text" name="Email" required/>
         </div>
         <div class="w3-section">
           <label>Message</label>
-          <input class="w3-input w3-border" type="text" name="Message" required>
+          <input class="w3-input w3-border" type="text" name="Message" required/>
         </div>
         <button type="submit" class="w3-button w3-black w3-margin-bottom"><i
             class="fa fa-paper-plane w3-margin-right"></i>Send Message</button>
